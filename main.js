5d7db24d19b4514c637d23566ac4be1362058a56
PedroDeLima210402 <pedro.limao@eaportal.org>
2025-06-08 19:20:30 -0300

    adição da verificação da input para não criar itens sem nome

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-
 const items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
+
+    if(itemName === ""){
+        alert("Digite um item válido!")
+        return
+    }
 
     const item = {
         name: itemName, 
