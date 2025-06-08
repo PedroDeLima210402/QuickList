f2820a97a1bdf83ba7c38c5f2ac8474962bcfbab
PedroDeLima210402 <pedro.limao@eaportal.org>
2025-06-08 18:41:57 -0300

    continuação da função de criar novos itens criando o objeto de cada item para ser adicionado no array de item

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-
 const items = []
 
 function addItme(){
     const itemName = document.querySelector("#item").value
 
+    const item = {
+        name: itemName,
+        checked:false
+    }
+
+    items.push(item)
+
+    
 }
