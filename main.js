ef2499ce030161023d5c3637fb1a273afdad6ad0
PedroDeLima210402 <pedro.limao@eaportal.org>
2025-06-08 18:57:18 -0300

    adição da funcionalidade de remover a classe hide-warning e também do setTimeout para adicionar novamente a classe hide-warning

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const items = []
 
 function addItem(){
@@ -51,6 +52,14 @@ function showItemsList(){
 
 function removeItem(itemName) {
     const itemIndex = items.findIndex((item) => item.name === item.name)
+    const divWarning = document.querySelector(".warning")
+    
+    divWarning.classList.remove("hide-warning")
+
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
+
 
     if(itemIndex !== -1) {
         items.splice(itemIndex, 1)
