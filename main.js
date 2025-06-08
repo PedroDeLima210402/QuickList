5713d1278273d0ef7d0566af1e1f6a01e1adfca7
PedroDeLima210402 <pedro.limao@eaportal.org>
2025-06-08 19:11:28 -0300

    verificação na input se o checked e verdadeiro ou não

--- main.js
+++ main.js
@@ -1,65 +1,54 @@
 
 const items = []
 
-function addItem(){
+function addItem() {
     const itemName = document.querySelector("#item").value
 
     const item = {
-        name: itemName,
-        checked:false
+        name: itemName, 
+        checked: false
     }
 
     items.push(item)
 
     document.querySelector("#item").value = ""
 
-
     showItemsList()
-
-    
 }
 
-function showItemsList(){
+function showItemsList() {
     const sectionList = document.querySelector(".list")
     sectionList.textContent = ""
 
-    items.map((item, index) => { 
-
+    items.map((item, index) => {
         sectionList.innerHTML += `
-         <div class="item">
+                <div class="item">
+                    <div>
+                        <input type="checkbox" name="list" id="item-${index}" ${item.checked && "checked"}>
 
-               <div class="item">
-            <div>
-                <input type="checkbox" name="list"  id="item-${index}">
+                        <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+                            <img src="./assets/checked.svg" alt="checked">
+                        </div>
+                        <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+                    </div>
 
-                <div class="custom-checkbox">
-                    <img src="./assets/checked.svg" alt="checked">
+                    <button onclick="removeItem('${item.name}')">
+                        <img src="./assets/trash-icon.svg" alt="trash icon">
+                    </button>
                 </div>
-
-                <label for="item-${index}">${item.name}</label>
-            </div>
-
-            <button>
-                <img src="./assets/trash-icon.svg" alt="trash-icon">
-            </button>
-
-        </div>
-        `  
+        `
     })
-
-
 }
 
 function removeItem(itemName) {
-    const itemIndex = items.findIndex((item) => item.name === item.name)
+    const itemIndex = items.findIndex((item) => item.name === itemName)
     const divWarning = document.querySelector(".warning")
-    
+
     divWarning.classList.remove("hide-warning")
 
     setTimeout(() => {
         divWarning.classList.add("hide-warning")
     }, 4000)
-
 
     if(itemIndex !== -1) {
         items.splice(itemIndex, 1)
@@ -70,7 +59,6 @@ function removeItem(itemName) {
 
 function checkItem(itemName) {
     const item = items.find((item) => item.name === itemName)
-
     item.checked = !item.checked
     showItemsList()
 }
